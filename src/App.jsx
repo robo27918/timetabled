bd8e13159fd22f97ee3c2176a5e008ed44c72fc0
Roberto Toribio <roberttoribio04@gmail.com>
2023-09-08 11:20:59 -0700

    added picture

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Calendar from './components/Calendar';
 const App = () => {
   return (
     <div className="App">
+      <img src="https://www.flysanjose.com/sites/default/files/sites/default/files/blog-photos/Mexico-City-Depositphotos_13449494_edited.jpg"/>
       <h1>Seven Days in Mexico City  🇲🇽</h1>
       <h2> Welcome fellow traveler! Check out this calendar to get 
         to know the city and see all the sights during your stay
